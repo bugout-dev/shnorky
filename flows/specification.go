1f3c72b093c9a610d8ac6aaee5b9623d690d1afd
Neeraj Kashyap <nkashy1@gmail.com>
2020-01-17 10:49:17 -0800

    JSON field name annotations for FlowSpecification

--- flows/specification.go
+++ flows/specification.go
@@ -10,11 +10,11 @@ import (
 // FlowSpecification - struct specifying a simplex data processing flow
 type FlowSpecification struct {
 	// Steps indexes each step in the flow and maps step names to component IDs
-	Steps map[string]string
+	Steps map[string]string `json:"steps"`
 	// Dependencies has step names as its keys and the corresponding value are the names of steps
 	// that the key step depends on. Steps which have no dependencies need not be included in this
 	// map
-	Dependencies map[string][]string
+	Dependencies map[string][]string `json:"dependencies"`
 }
 
 // ReadSingleSpecification reads a single ComponentSpecification JSON document and returns the
